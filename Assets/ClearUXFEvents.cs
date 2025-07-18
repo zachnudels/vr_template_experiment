3d062b9066595bdef2acec453557567acdc7f420
Lab A429 <vrlab.ibba@gmail.com>
2025-07-18 16:51:54 +0200

    Add

---
+++ Assets/ClearUXFEvents.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UXF;
+
+public class ClearUXFEvents : MonoBehaviour
+{
+    //public Session session;
+
+    void Start()
+    {
+        //session.OnExperimentBegin.RemoveAllListeners();
+        //session.OnTrialBegin.RemoveAllListeners();
+        //session.OnTrialEnd.RemoveAllListeners();
+        //session.OnExperimentEnd.RemoveAllListeners();
+
+        //Debug.Log("Cleared UXF Session events to fix inspector crash.");
+    }
+}
